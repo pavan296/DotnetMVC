92e14495ce2f3d5926fa11308c7d5fffe8af2f7f
Pavan007 <pavanbm17@gmail.com>
2021-12-07 12:31:00 +0530

    Add project files.

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+                {
+                    new Restaurant{Id=1 , Name="Scott-Pizza" , Cuisine=CuisineType.Italian},
+                    new Restaurant{Id=2 , Name="Masala-dosa" , Cuisine=CuisineType.Indian},
+                    new Restaurant{Id=3 , Name="idli" , Cuisine=CuisineType.French}
+                    //new Restaurant{Id=4 , Name="puliyogre" , Cuisine=CuisineType.None}
+                };
+
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
