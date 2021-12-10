e81f011de0d4eea277121c963bfa123656490f33
Pavan007 <pavanbm17@gmail.com>
2021-12-10 12:30:35 +0530

    10/12 Db context

--- OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -11,7 +11,9 @@ namespace OdeToFood.Data.Models
     {
         public int Id { get; set; }
         [Required]
+
         public string Name { get; set; }
+        [Display(Name ="Type of food")]
         public CuisineType Cuisine { get; set; }
     }
 }
